f5c5ca40a3697eccb4467835e54538c98a118ded
Samuel Li <lisamuel1990@gmail.com>
2017-05-19 19:45:56 -0400

    paint and palette function add

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,13 @@
 var u = new SpriteUtilities(PIXI);
 var renderer, state;
 var grid = new Container(),
+    palette = new Container(),
     stage = new Container();
 
-var width, height, tile_size, border;
+//Options
+var grid_lines = true;
+
+var width, height, tile_size, border, palette_selected;
 
 
 function main_init(form){
@@ -14,21 +18,35 @@ function main_init(form){
     border = 2;
 
     //Set stage
-    renderer = autoDetectRenderer (width * tile_size, height * tile_size);
+    //250px for palette
+    renderer = autoDetectRenderer (width * tile_size + 250, height * tile_size);
     renderer.backgroundColor = 0xFFFFFF;
 
     //Move hero card
     var el = document.querySelector(".hero-card");
     el.classList.add("move-up");
     
-    //Create map
     create_map();
+    create_palette();
 
     setTimeout(function(){ 
         document.getElementById("ui-container").classList.add('u-hidden'); 
         document.body.appendChild(renderer.view);
+        renderer.render(stage);
     }, 500);
     
+}
+function create_palette(){
+    for (var i = 0; i < textureMap.length; i++) {
+        var sprite = u.sprite(textureMap, tile_size * i, 0);
+        sprite.gotoAndStop(i);
+        sprite.interactive = true;
+        sprite.buttonMode = true;
+        sprite.on('pointerdown', setPalette);
+        palette.addChild(sprite);
+    }
+    palette.x = width * tile_size + tile_size;
+    stage.addChild(palette);
 }
 
 function create_map(){
@@ -39,23 +57,52 @@ function create_map(){
         var sprite = u.sprite(textureMap, tile_size * i, tile_size * k);
             sprite.interactive = true;
             sprite.buttonMode = true;
-            sprite.on('pointerdown', onClick);
+            sprite.on('pointerdown', paint);
             grid.addChild(sprite);
         }
     }
-    
-    grid.addChild(sprite);
+    //grid.interactive = true;
+    //grid.buttonMode = true;
     stage.addChild(grid);
+}
 
+function setPalette() {
+    if(palette_selected == undefined) {
+        this.tint = 0x36B29E;
+        palette_selected = this.currentFrame;
+        renderer.render(stage);
+        return;
+    }
+    if(palette_selected != this.currentFrame) {
+        //Reset tint of prev selected palette
+        palette.getChildAt(palette_selected).tint = 0xFFFFFF;
+        this.tint = 0x36B29E;
+        palette_selected = this.currentFrame;;
+    }
+    
     renderer.render(stage);
 }
 
-//CLick functions
+function paint() {
+    if(palette_selected == undefined)
+        return;
 
-function onClick() {
-
-    this.setTexture(textureMap[1]);
+    this.gotoAndStop(palette_selected);
     renderer.render(stage);
+}
+
+function export_grid() {
+    var array = [];
+    var string = "["
+    for (var i = 0; i < grid.children.length; i++) {
+        //array.push(grid.getChildAt(i).currentFrame);
+        string += grid.getChildAt(i).currentFrame;
+        if(i < grid.children.length - 1)
+            string += ','
+    }
+
+    string += "]";
+    return(string);
 }
 
 //Need to find a way to tag a texture
--- src/scripts/textures.js
+++ src/scripts/textures.js
@@ -1,9 +1,25 @@
 var textureMap = [];
-var colorArray = [0xFFFFFF, 0xD7E15F];
+var colorArray = [0x333333, 0xFFFFFF, 0xFEE5CB, 0xC1C1C1, 0xD7E15F];
 
 function createTextureMap() {
 
     for (var i = 0; i < colorArray.length; i++){
+        (typeof colorArray[i]);
+        var graphic = new PIXI.Graphics();
+            graphic.beginFill(colorArray[i], 1);
+            if(grid_lines)
+                graphic.lineStyle(border, 0xCAA555);
+            graphic.drawRect(0,0, tile_size, tile_size);
+
+        var texture = renderer.generateTexture(graphic);
+
+        textureMap.push(texture);
+    }
+}
+
+function createPaletteMap() {
+    for (var i = 0; i < colorArray.length; i++){
+        (typeof colorArray[i]);
         var graphic = new PIXI.Graphics();
             graphic.lineStyle(border, 0xCAA555);
             graphic.beginFill(colorArray[i], 1);
@@ -13,4 +29,5 @@ function createTextureMap() {
 
         textureMap.push(texture);
     }
+
 }
